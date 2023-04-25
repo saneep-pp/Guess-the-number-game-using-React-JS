92a29e0714bb83380a375a20c66ad58ff36f56a7
sani-coder <saneeppp@gmail.com>
2023-04-25 18:06:38 +0530

    v2.0 Completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count,setCount]=useState(1);
-  const [score,setScore]=useState(10);
+  const [count, setCount] = useState(1);
+  const [score, setScore] = useState(10);
   const checkNumber = () => {
-  
     let num = Number(document.getElementById("input").value);
     let random = Math.floor(Math.random() * (10 - 0)) + 1;
-    
+
     console.log(random);
     console.log(num);
     if (num === "" || num === null) {
@@ -16,100 +15,64 @@ function App() {
     } else {
       if (random === num) {
         alert("Congrats......! You Won.");
-   
-        setScore(score+10);
-        document.getElementById("score").innerHTML=score;
 
-        
-       if(score===50){
-        setCount(count+1);
-       }
-       else if(score===100)
-       {
-        setCount(count+1);
-       }
-       else if(score===200){
-        setCount(count+1);
-       }
-       else if(score===250){
-        setCount(count+1);
-       }
-       else if(score===300){
-        setCount(count+1);
-       }
-       else if(score===350){
-        setCount(count+1);
-       }
-       else if(score===400){
-        setCount(count+1);
-       }
-       else if(score===450){
-        setCount(count+1);
-       }
-       else if(score===500){
-        setCount(count+1);
-       }
-        if(count===2){
-          document.getElementById("level").innerHTML="Level 2";
-          
-  
+        setScore(score + 10);
+        document.getElementById("score").innerHTML = score;
+
+        if (score === 50) {
+          setCount(count + 1);
+        } else if (score === 100) {
+          setCount(count + 1);
+        } else if (score === 200) {
+          setCount(count + 1);
+        } else if (score === 250) {
+          setCount(count + 1);
+        } else if (score === 300) {
+          setCount(count + 1);
+        } else if (score === 350) {
+          setCount(count + 1);
+        } else if (score === 400) {
+          setCount(count + 1);
+        } else if (score === 450) {
+          setCount(count + 1);
+        } else if (score === 500) {
+          setCount(count + 1);
         }
-        else if(count===3){
-          document.getElementById("level").innerHTML="Level 3";
-          
+        if (count === 2) {
+          document.getElementById("level").innerHTML = "Level 2";
+        } else if (count === 3) {
+          document.getElementById("level").innerHTML = "Level 3";
+        } else if (count === 4) {
+          document.getElementById("level").innerHTML = "Level 4";
+        } else if (count === 5) {
+          document.getElementById("level").innerHTML = "Level 5";
+        } else if (count === 6) {
+          document.getElementById("level").innerHTML = "Level 6";
+        } else if (count === 7) {
+          document.getElementById("level").innerHTML = "Level 7";
+        } else if (count === 8) {
+          document.getElementById("level").innerHTML = "Level 8";
+        } else if (count === 9) {
+          document.getElementById("level").innerHTML = "Level 9";
+        } else if (count === 10) {
+          document.getElementById("level").innerHTML = "Level 10";
         }
-        else if(count===4){
-          document.getElementById("level").innerHTML="Level 4";
-          
-        }
-        else if(count===5){
-          document.getElementById("level").innerHTML="Level 5";
-          
-        }
-        else if(count===6){
-          document.getElementById("level").innerHTML="Level 6";
-          
-        }
-        else if(count===7){
-          document.getElementById("level").innerHTML="Level 7";
-          
-        }
-        else if(count===8){
-          document.getElementById("level").innerHTML="Level 8";
-          
-        }
-        else if(count===9){
-          document.getElementById("level").innerHTML="Level 9";
-          
-        }
-        else if(count===10){
-          document.getElementById("level").innerHTML="Level 10";
-          
-        }
-       
-        
-       }
-       else {
+      } else {
         alert("Your Guessing is not correct. Try again...");
-        
-       }
-
-       
       }
-      
+    }
   };
   return (
     <div className="App">
       <header className="App-header">
         <h2>Guess The Number</h2>
       </header>
-      
-      <div className="body">
 
+      <div className="body">
         <h2 id="level">Level 1</h2>
         <div className="res">
-        Score<p id="score">0</p>
-      </div>
+          Score<p id="score">0</p>
+        </div>
         <input
           id="input"
           type="number"
